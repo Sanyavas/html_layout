f0278e9bb183f871c493f242788ad474380f72b0
Sanyavas <vasilinaoleksandr@gmail.com>
2024-11-13 23:40:00 +0200

    add icons dark theme

--- js/dark_theme.js
+++ js/dark_theme.js
@@ -1,15 +1,20 @@
 // Отримуємо кнопку перемикання теми
 const themeToggleButton = document.getElementById('theme-toggle-btn');
 
+// Отримуємо контейнер кнопки для перемикання теми
+const themeToggleContainer = themeToggleButton.parentElement;
+
 // Перевіряємо, чи вже збережена темна тема в localStorage
 if (localStorage.getItem('theme') === 'dark') {
   document.body.classList.add('dark-theme');
+  themeToggleContainer.classList.add('dark'); // Додаємо клас на контейнер
 }
 
 // Додаємо обробник події для натискання на кнопку
 themeToggleButton.addEventListener('click', () => {
   // Перемикаємо клас `dark-theme` на body
   document.body.classList.toggle('dark-theme');
+  themeToggleContainer.classList.toggle('dark'); // Перемикаємо клас на контейнері
 
   // Зберігаємо стан теми в localStorage
   if (document.body.classList.contains('dark-theme')) {
