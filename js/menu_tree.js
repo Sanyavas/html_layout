aec96a871db8ca5f4b06125f0ce367d852b4cb8d
Sanyavas <vasilinaoleksandr@gmail.com>
2024-11-14 17:54:27 +0200

    add tree menu

---
+++ js/menu_tree.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Знаходимо всі кнопки, що мають клас submenu-toggle
+  const submenuButtons = document.querySelectorAll('.submenu-toggle');
+
+  // Встановлюємо обробник подій для кожної кнопки
+  submenuButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      toggleSubmenu(button);
+    });
+  });
+
+  // Функція відкривання/закривання підменю
+  function toggleSubmenu(button) {
+    const submenu = button.nextElementSibling;
+    if (submenu) {
+      submenu.style.display = submenu.style.display === 'block' ? 'none' : 'block';
+    }
+  }
+});
